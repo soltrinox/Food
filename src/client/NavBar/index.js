d9c6bd82530a48cd40b838f374393c5ab4228f94
krishnaUIDev <krishna.kondoju007@gmail.com>
2020-10-14 18:38:16 -0400

    'added'

--- src/client/NavBar/index.js
+++ src/client/NavBar/index.js
@@ -5,7 +5,7 @@ const TopNav = () => {
   const gallery = window.location.pathname.includes("gallery");
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
-      <Navbar.Brand href="/">Food Images</Navbar.Brand>
+      <Navbar.Brand href="/">What-A-Burger</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
