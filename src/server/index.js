22e5ca3b3f12aa8dc30966996c8e99228e8c886b
krishnaUIDev <krishna.kondoju007@gmail.com>
2020-10-15 00:33:58 -0400

    'added'

--- src/server/index.js
+++ src/server/index.js
@@ -2,10 +2,22 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const path = require("path");
+// const { graphqlHTTP } = require("express-graphql");
+// const { buildSchema } = require("graphql");
 
 require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/prod")(app);
+
+// graphql schema
+
+// const schema = buildSchema(`
+//  type Query {
+//    hello: String
+//  }
+// `);
+
+//const root = { hello: () => "hello world" };
 
 app.set("view engine", "pug");
 app.set("views", "./views"); // to set default template
@@ -15,6 +27,15 @@ app.use(express.static(path.join(__dirname, "dist")));
 app.use("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../../dist/index.html"));
 });
+
+// app.use(
+//   "/graphql",
+//   graphqlHTTP({
+//     schema,
+//     rootValue: root,
+//     graphiql: true,
+//   })
+// );
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port ${port}!`));
