908b67f9374d42da2912dbfaea7eb5742bec0a94
krishnaUIDev <krishna.kondoju007@gmail.com>
2020-10-16 00:11:23 -0400

    'added'

--- src/server/routes/employee.js
+++ src/server/routes/employee.js
@@ -5,22 +5,34 @@ const { Employee } = require("../models/employee.models");
 router.get("/", async (req, res) => {
   const employeeId = req.query.employeeId;
   const records = req.query.records;
-  const name = req.query.name;
+  const serachKey = req.query.search;
   let user;
   if (employeeId) {
     user = await Employee.find({ empId: employeeId });
   } else if (records) {
     user = await Employee.find({}).limit(parseInt(records, 10));
-  } else if (name && name.length > 3) {
-    user = await Employee.find({ FirstName: { $regex: /name/i } });
+  } else if (serachKey && serachKey.length >= 3) {
+    let regex = new RegExp(serachKey, "i");
+    user = await Employee.find({
+      $or: [
+        { FirstName: { $regex: regex } },
+        { LastName: { $regex: regex } },
+        { PhoneNumber: { $regex: regex } },
+      ],
+    });
   } else {
-    user = await Employee.find({});
+    if (!serachKey) user = await Employee.find({});
   }
+
   if (!user) {
     res.status(400).send({
-      message: `Could not find any record with employee Id ${req.query.employeeId}`,
+      message: `Could not find any record with key :${
+        serachKey ? serachKey : employeeId
+      }`,
     });
   }
+  let recordCount = user && user.length;
+  console.log(`fetched : ${recordCount}`);
   res.send(user);
 });
 
