94e8e2ceeded6966e7fba5145985763eb3a7e9ef
krishnaUIDev <krishna.kondoju007@gmail.com>
2020-10-17 00:31:43 -0400

    'added'

--- src/server/startup/routes.js
+++ src/server/startup/routes.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+const responseTime = require("response-time");
+//const StatsD = require('node-statsd')
+
 const getFoodItems = require("../routes/getFoodItems");
 const employee = require("../routes/employee");
 
@@ -8,7 +11,7 @@ module.exports = (app) => {
   app.use(express.json({ limit: "50mb" })); // req.body
   app.use(express.urlencoded({ limit: "50mb", extended: true })); // to loges the reqest into terminal
   app.use(cors());
-
+  app.use(responseTime());
   app.use("/api/getFoodItems", getFoodItems);
   app.use("/api/employee", employee);
 };
