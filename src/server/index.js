5defac24562b89c2e37d144ce5f05ac1e86ed05e
krishnaUIDev <krishna.kondoju007@gmail.com>
2020-10-18 14:23:35 -0400

    'added'

--- src/server/index.js
+++ src/server/index.js
@@ -2,46 +2,28 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const path = require("path");
-
-// const { graphqlHTTP } = require("express-graphql");
-// const { buildSchema } = require("graphql");
+//var cron = require("node-cron");
 
 require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/prod")(app);
 
-// graphql schema
-
-// const schema = buildSchema(`
-//  type Query {
-//    hello: String
-//  }
-// `);
-
-//const root = { hello: () => "hello world" };
+// cron.schedule("* * * * * *", () => {
+//   console.log("running a task every minute");
+// });
 
 process.on("unhandledRejection", (ex) => {
-  // process.exit(1);
   throw ex;
 });
 
 app.set("view engine", "pug");
 app.set("views", "./views"); // to set default template
 
-app.use(express.static(path.join(__dirname, "dist")));
+// app.use(express.static(path.join(__dirname, "dist")));
 
-app.use("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../dist/index.html"));
-});
-
-// app.use(
-//   "/graphql",
-//   graphqlHTTP({
-//     schema,
-//     rootValue: root,
-//     graphiql: true,
-//   })
-// );
+// app.use("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../../dist/index.html"));
+// });
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port ${port}!`));
