ed736a307b5af52a9eb280d8cbf5cd26bde2edb2
krishnaUIDev <krishna.kondoju007@gmail.com>
2020-10-19 23:18:24 -0400

    added colors

--- src/server/index.js
+++ src/server/index.js
@@ -2,14 +2,15 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const colors = require("colors");
 
+const connectDB = require("./startup/db");
 // Dev logging middleware
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
-
+connectDB();
 require("./startup/routes")(app);
-require("./startup/db")();
 require("./startup/prod")(app);
 // data jobs
 require("./datajobs/cron")();
@@ -18,4 +19,14 @@ app.set("view engine", "pug");
 app.set("views", "./views"); // to set default template
 
 const port = process.env.PORT || 8080;
-app.listen(port, () => console.log(`Listening on port ${port}!`));
+const server = app.listen(port, () =>
+  console.log(`Listening on port ${port}`.yellow.bold)
+);
+
+// Handled unhandled promise rejections
+
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Err: Unhandled Rejection ${err.message}`.red.bold);
+  // Close server and exist
+  server.close(() => process.exit(1));
+});
--- src/server/startup/db.js
+++ src/server/startup/db.js
@@ -3,13 +3,14 @@ const mongoose = require("mongoose");
 
 const { MONGO_CONNECTION } = process.env;
 
-module.exports = () => {
-  mongoose
-    .connect(MONGO_CONNECTION, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .then(() => winston.info("conneted to mogodb.."))
-    .catch((err) => winston.info("Failed to Connect database"));
-  mongoose.set("useCreateIndex", true);
+const connectDB = async () => {
+  const conn = await mongoose.connect(MONGO_CONNECTION, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  });
+  winston.info(`MongoDB connectd ${conn.connection.host}`);
 };
+
+module.exports = connectDB;
