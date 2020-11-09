c3827c6695be72cd8110e13d03f88a2552b18dc6
krishnaUIDev <krishna.kondoju007@gmail.com>
2020-11-08 21:34:44 -0500

    added logout clear cookie

--- src/server/controllers/auth.js
+++ src/server/controllers/auth.js
@@ -48,6 +48,21 @@ exports.login = asyncHandler(async (req, res, next) => {
   sendTokenResponse(user, 200, res);
 });
 
+// @desc   Log out user
+// @route  POST /api/v1/auth/logout
+// @access  Private
+
+exports.logout = asyncHandler(async (req, res, next) => {
+  res.cookie("token", "none", {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true,
+  });
+  res.status(200).json({
+    success: true,
+    data: {},
+  });
+});
+
 // @desc   current login user
 // @route  POST /api/v1/auth/me
 // @access  Private
--- src/server/middleware/auth.js
+++ src/server/middleware/auth.js
@@ -14,10 +14,9 @@ exports.protect = asyncHandler(async (req, res, next) => {
     // Set token from Bearer token in header
     token = authorization.split(" ")[1];
     // Set token from cookie
+  } else if (req.cookies.token) {
+    token = req.cookies.token;
   }
-  // else if (req.cookies.token) {
-  //   token = req.cookies.token;
-  // }
 
   // Make sure token exists
   if (!token) {
